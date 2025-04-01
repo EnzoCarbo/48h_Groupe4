9e40ab9feb1bd8976363c5df9df3ffe20afbb4b3
oumniaDEV27 <ahlem.elmokretar@ynov.com>
2025-04-01 09:41:46 +0200

    tic tac toe fonctionnel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import TicTacToe from './TicTacToe'; // Import du composant TicTacToe
 import reportWebVitals from './reportWebVitals';
-import App from './App.css';
+import "./TicTacToe.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
